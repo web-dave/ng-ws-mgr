c444e2b1af111c54e30f624d2ed499f7639fb6c1
Dave <dave@webdave.de>
2020-11-11 11:45:06 +0100

    hurz

--- src/app/fleet/vehicle-details/vehicle-details.component.ts
+++ src/app/fleet/vehicle-details/vehicle-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject, Subscription } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { switchMap, takeUntil } from 'rxjs/operators';
 import { IVehicle } from '../models/ivehicle';
 import { FleetService } from '../shared/fleet.service';
@@ -10,21 +10,14 @@ import { FleetService } from '../shared/fleet.service';
   templateUrl: './vehicle-details.component.html',
   styleUrls: ['./vehicle-details.component.scss'],
 })
-export class VehicleDetailsComponent implements OnInit, OnDestroy {
-  vehicle: IVehicle;
-  end$ = new Subject<number>();
+export class VehicleDetailsComponent implements OnInit {
+  car$: Observable<IVehicle>;
 
   constructor(private route: ActivatedRoute, private service: FleetService) {}
 
   ngOnInit(): void {
-    this.route.params
-      .pipe(
-        switchMap((params) => this.service.getCar(params.id)),
-        takeUntil(this.end$)
-      )
-      .subscribe((data) => (this.vehicle = data));
-  }
-  ngOnDestroy() {
-    this.end$.next(1);
+    this.car$ = this.route.params.pipe(
+      switchMap((params) => this.service.getCar(params.id))
+    );
   }
 }
