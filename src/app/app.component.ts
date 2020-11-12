71c39f652e83e6b9d95f6874f5c7cedce1b069ef
Dave <dave@webdave.de>
2020-11-12 14:30:54 +0100

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'bka-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent {
   title = 'fleet-managrrrrrr';
